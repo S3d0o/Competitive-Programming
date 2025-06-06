dfb3cead6706266e3ae7db9259fecc801566d862
S3d0o <s3d0o.xn@gmail.com>
2025-06-06 20:26:15 +0300

    Create Draw_a_Square.cs

---
+++ Codeforces/Div 3/Draw_a_Square.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp26
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int test = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < test; i++)
+            {
+                string[] input = Console.ReadLine().Split(' ');
+                int l = int.Parse(input[0]);
+                int r = int.Parse(input[1]);
+                int d = int.Parse(input[2]);
+                int u = int.Parse(input[3]);
+
+                // The square condition: All four sides must be equal.
+                if (l == r && r == d && d == u)
+                {
+                    Console.WriteLine("Yes");
+                }
+                else
+                {
+                    Console.WriteLine("No");
+                }
+            }
+        }
+    }
+}
